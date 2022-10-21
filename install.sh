a1b3684b7dcbf57b5a57053c5ca81d8e951f8749
Yusuke Higuchi <higuchi@YusukenoMacBook-Pro.local>
2022-10-21 16:55:20 +0900

    Update

--- install.sh
+++ install.sh
@@ -4,18 +4,22 @@
 cd ~
 
 # homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew doctor
 
 # zsh
-brew install zsh 
+brew install zsh
+# zplug
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
 sudo chsh -s $(which zsh) $USER
 # tmux, vim(+clipboard), iterm2, git
 brew install git tmux vim git
-brew cask install iterm2
+brew install --cask iterm2
 # font: mplus
-brew tap caskroom/homebrew-fonts
-brew cask install font-m-plus
+brew tap homebrew/cask-fonts
+brew install --cask font-m-plus-1
+brew install --cask font-m-plus-1-code
 
 # dotfiles
 git clone https://github.com/higuri/dotfiles
@@ -23,7 +27,11 @@ pushd dotfiles
   . install.sh
 popd
 
+# some settings...
+# https://stackoverflow.com/questions/39606031/intellij-key-repeating-idea-vim
+defaults write -g ApplePressAndHoldEnabled -bool false
+
 # XXX: iTerm2 Preferences
 # - Profiles > Colors > Color Presets: Solarized Dark
-# - Profiles > Text > Font: M+ 1mn medium 16pt
+# - Profiles > Text > Font: M PLUS 1 Code 16pt
 # - Profiles > Window > Columns x Rows: 160 x 40
